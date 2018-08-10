3be18db232013690fdc98cd63a91d7c6744ef970
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-10 23:11:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
       @user = User.find_by(:username => params[:username])
-      if @user && @user.password_digest == params[:password_digest]
+      if @user && @user.password_digest == params[:password]
         session[:user_id] = @user.id
         redirect to "/account"
       else
