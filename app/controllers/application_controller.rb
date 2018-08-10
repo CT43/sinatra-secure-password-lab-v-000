cb3ca2345e46c125abce78267fdad39bbf2660e1
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-10 22:38:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(:username => params[:username])
      if user && user.password == params[:password]
-       redirect "/login"
+       redirect "/account"
      else
        redirect "/failure"
      end
